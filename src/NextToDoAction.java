d9756fa722255b6a72feb4bba1ce3c8cacffa519
hbt <hassenbentanfous@gmail.com>
2018-12-28 15:21:06 -0500

    init

---
+++ src/NextToDoAction.java
@@ -0,0 +1,11 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class NextToDoAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+        System.out.println("asdw");
+    }
+}
